e8573e9fa6144888365f700e52c6c582e996c573
Nikola Whallon <nikola.whallon@gmail.com>
2026-04-15 08:51:11 -0700

    better barista spawning code

--- scenes/arena.gd
+++ scenes/arena.gd
@@ -19,6 +19,18 @@ var right_score = 0
 func _process(_delta: float) -> void:
 	if state == State.STARTING:
 		state = State.PLAYING
+		if multiplayer.is_server():
+			for peer in NodeUtils.get_first_ancestor_in_group_for_node(self, "App").get_peer_ids_for_match(match_id):
+				if DisplayServer.get_name() == "headless" and peer == 1:
+					continue
+				if len(NodeUtils.get_nodes_in_group_for_node(self, "Barista")) % 2 == 0:
+					var barista = load("res://scenes/barista.tscn").instantiate()
+					barista.init(peer, "left", $LeftCafe.position)
+					$Replicated.add_child(barista, true)
+				else:
+					var barista = load("res://scenes/barista.tscn").instantiate()
+					barista.init(peer, "right", $RightCafe.position)
+					$Replicated.add_child(barista, true)
 
 	if Input.is_action_just_pressed("leave"):
 		emit_signal("leave_requested")
@@ -26,36 +38,9 @@ func _process(_delta: float) -> void:
 	if not multiplayer.is_server():
 		return
 
-func _input(event):
-	if event is InputEventJoypadButton:
-		for barista in NodeUtils.get_nodes_in_group_for_node(self, "Barista"):
-			if barista.peer_id == multiplayer.get_unique_id():
-				return
-
-		request_spawn_barista.rpc_id(1)
-
-	if event is InputEventKey and event.is_pressed():
-		if event.keycode == KEY_SPACE or event.keycode == KEY_UP or event.keycode == KEY_DOWN or event.keycode == KEY_LEFT or event.keycode == KEY_RIGHT:
-			for barista in NodeUtils.get_nodes_in_group_for_node(self, "Barista"):
-				if barista.peer_id == multiplayer.get_unique_id():
-					return
-
-			request_spawn_barista.rpc_id(1)
-
 @rpc("any_peer", "reliable")
 func announce_start_game(_random_seed, _peers):
 	state = State.STARTING
-
-@rpc("any_peer", "call_local", "reliable")
-func request_spawn_barista():
-	var cafe = "left"
-	for barista in NodeUtils.get_nodes_in_group_for_node(self, "Barista"):
-		if barista.cafe == "left":
-			cafe = "right"
-
-	var barista = load("res://scenes/barista.tscn").instantiate()
-	barista.init(multiplayer.get_remote_sender_id(), cafe, Vector2.ZERO)
-	$Replicated.add_child(barista, true)
 
 @rpc("authority", "reliable")
 func announce_update_left_score(new_score: int) -> void:
